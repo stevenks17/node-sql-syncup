ade972ee42a07eda88d01576f3ec7755115a3d9a
stevenks17 <stevenks17@gmail.com>
2022-02-07 19:38:05 -0500

    server setup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const server = require ("./server");
+const server = require ( "./server" );
 
 const startServer = async () => {
-    try {
-        const config = {
-           host: "localhost" 
-           port: 8080
-        };
+	try {
+		const config = {
+			host: "localhost",
+			port: 8080
+		};
 
-        const app = await server(config);
-        await app.start();
+		const app = await server( config );
+		await app.start();
 
-        console.log(`Server running at http://${config.host}:{config.port} `);
-    }
+		console.log( `Server running at http://${ config.host }:${ config.port } ` );
+	}
 
-    catch( err ) {
-        console.log("startup error", err);
-    }
+	catch( err ) {
+		console.log( "startup error", err );
+	}
 };
 
 startServer();
